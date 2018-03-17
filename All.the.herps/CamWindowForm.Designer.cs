337b56dc6d3375bdeda79eb13ff3265ffe5f0035
Jóhann <johann@gagecannon.com>
2018-03-17 02:09:32 +0000

    le code

---
+++ All.the.herps/CamWindowForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace All.the.herps
+{
+    partial class CamWindowForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetCams = new System.Windows.Forms.Button();
+            this.btnScreenshot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGetCams
+            // 
+            this.btnGetCams.Location = new System.Drawing.Point(12, 21);
+            this.btnGetCams.Name = "btnGetCams";
+            this.btnGetCams.Size = new System.Drawing.Size(116, 38);
+            this.btnGetCams.TabIndex = 0;
+            this.btnGetCams.Text = "available cams";
+            this.btnGetCams.UseVisualStyleBackColor = true;
+            this.btnGetCams.Click += new System.EventHandler(this.btnGetCams_Click);
+            // 
+            // btnScreenshot
+            // 
+            this.btnScreenshot.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnScreenshot.Location = new System.Drawing.Point(1290, 21);
+            this.btnScreenshot.Name = "btnScreenshot";
+            this.btnScreenshot.Size = new System.Drawing.Size(115, 37);
+            this.btnScreenshot.TabIndex = 1;
+            this.btnScreenshot.Text = "Download screenshot";
+            this.btnScreenshot.UseVisualStyleBackColor = true;
+            this.btnScreenshot.Visible = false;
+            this.btnScreenshot.Click += new System.EventHandler(this.btnScreenshot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 589);
+            this.Controls.Add(this.btnScreenshot);
+            this.Controls.Add(this.btnGetCams);
+            this.Name = "Form1";
+            this.Text = "The herp derp camera thingie";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetCams;
+        private System.Windows.Forms.Button btnScreenshot;
+    }
+}
+
---
+++ All.the.herps/CamWindowForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Windows.Forms;
+using DirectShowLib;
+using Emgu.CV;
+using Emgu.CV.UI;
+
+namespace All.the.herps
+{
+    public partial class CamWindowForm : Form
+    {
+        private VideoCapture _capture;
+        private DsDevice[] _SystemCameras;
+        private ImageViewer viewer;
+
+        public CamWindowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void createImageViewer()
+        {
+            viewer = new ImageViewer
+            {
+                Location = new Point(200, 21),
+                Width = 640,
+                Height = 480,
+                BackColor = Color.DeepPink,
+                TopLevel = false
+            };
+
+            viewer.VisibleChanged += viewerVisibleChange;
+
+            Controls.Add(viewer);
+        }
+
+        private void viewerVisibleChange(object sender, EventArgs e)
+        {
+            if (!viewer.Visible)
+            {
+                //TODO: trigger stop on the active camera button or some shit
+            }
+        }
+         /// <summary>
+         /// gets a list of the connected cams
+         /// </summary>
+         /// <param name="sender"></param>
+         /// <param name="e"></param>
+        private void btnGetCams_Click(object sender, EventArgs e)
+        {
+            List<KeyValuePair<int, string>> ListCamerasData = new List<KeyValuePair<int, string>>();
+
+            _SystemCameras = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+            int _DeviceIndex = 0;
+            foreach (DsDevice _Camera in _SystemCameras)
+            {
+                ListCamerasData.Add(new KeyValuePair<int, string>(_DeviceIndex, _Camera.Name));
+                _DeviceIndex++;
+            }
+
+            if (!ListCamerasData.Any())
+            {
+                MessageBox.Show("No cameras available, you twat");
+                return;
+            }
+            
+            int startTop = 62;
+            foreach (var cam in ListCamerasData)
+            {
+                Button btn = new Button
+                {
+                    Text = cam.Value,
+                    Name = $"camBtn{cam.Key}",
+                    Location = new Point(12, startTop),
+                    Size = new Size(116, 38),
+                    TabIndex = cam.Key + 1,
+                    UseVisualStyleBackColor = true
+                };
+                btn.Click += camBtnClick;
+                Controls.Add(btn);
+
+                startTop += 30;
+            }
+        }
+
+        /// <summary>
+        /// when camera button clicked. Duh ...
+        /// Only tested this with one integrated cam on laptop. Could jsut as well blow to shits if more cams are connected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void camBtnClick(object sender, EventArgs e)
+        {
+            var btn = (Button) sender;
+            var camIndex = Convert.ToInt32(btn.Name.Replace("camBtn", ""));
+            var orgCamBtnText = _SystemCameras[camIndex].Name;
+
+            if (btn.Text != "STOP")
+            {
+
+                createImageViewer();
+
+                viewer.Show();
+                btnScreenshot.Show();
+
+                _capture = new VideoCapture(camIndex);
+                _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.XiFramerate, 30);
+                _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, 480);
+                _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, 640);
+
+                btn.Text = "STOP";
+
+                Application.Idle += captureFrame;
+            }
+            else
+            {
+                viewer.Hide();
+                btnScreenshot.Hide();
+
+                Application.Idle -= captureFrame;
+
+                btn.Text = orgCamBtnText;
+
+                _capture.Stop();
+
+                _capture?.Dispose();
+            }
+        }
+
+        private void captureFrame(object sender, EventArgs e)
+        {
+            viewer.Image = _capture.QueryFrame();
+        }
+
+        /// <summary>
+        /// grabs a screenshot from the imageviewer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnScreenshot_Click(object sender, EventArgs e)
+        {
+            Image renaultCaptur = viewer.Image.Bitmap;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Images|*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(sfd.FileName);
+                
+                renaultCaptur.Save(string.IsNullOrEmpty(ext) ? $"{sfd.FileName}.jpg" : sfd.FileName, format);
+            }
+        }
+    }
+}
